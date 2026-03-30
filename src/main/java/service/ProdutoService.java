c947e064033ef16328122bbf7f2e12d7b29ff37d
Gabriel <gmdf10@gmail.com>
2026-03-30 15:34:15 -0300

    feat: adicionar List produtos e Map categorias

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Produto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ProdutoService {
+
+    private List<Produto> produtos = new ArrayList<>();
+    private Map<String, List<Produto>> categorias = new HashMap<>();
+
 }
