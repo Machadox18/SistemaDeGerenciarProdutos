a5fdc6eed8d44f251d295053012c0a347b2091ce
Gabriel <gmdf10@gmail.com>
2026-03-30 15:47:47 -0300

    feat: adiciona cadastro inicial de produtos na Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Produto;
+import service.ProdutoService;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ProdutoService service = new ProdutoService();
+
+        Produto p1 = new Produto("Mouse", 50, "Eletrônicos");
+        Produto p2 = new Produto("Cadeira", 120, "Móveis");
+        Produto p3 = new Produto("Xbox", 3500, "VídeoGame");
+
+        service.adicionarProduto(p1);
+        service.adicionarProduto(p2);
+        service.adicionarProduto(p3);
+
+        System.out.println("Todos os produtos");
+        service.listarProdutos();
     }
 }
